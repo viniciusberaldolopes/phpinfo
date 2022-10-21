a66dc99d792a26eef2acc091b2a8039efb158e35
viniciusberaldolopes <65480628+viniciusberaldolopes@users.noreply.github.com>
2022-10-21 09:17:38 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>FORMALÁRIO PARA INSCRIÇÃO DE COMPETIFDORES</p>
+
+    <form action="info.php" method="post">
+        <p> Seu nome: <input type="text" name="" id=""></p>
+        <p> Sua idade: <input type="text" name="" id=""></p>
+        <p><input type="submit"></p>
+    </form>
+    
+</body>
+</html>
