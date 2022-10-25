023a2168a0c6245da23f271184f3ef99e1fec300
viniciusberaldolopes <viniblp123@gmail.com>
2022-10-25 16:08:35 -0300

    adicionando condicionais no formulario

--- info.php
+++ info.php
@@ -6,8 +6,29 @@ $categorias[] = 'adolescentes';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
 
-$nome = 'Eduardo';
-$idade = 16 ;
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+if(empty($nome))
+{
+    echo 'o nome não pode ser vazio';
+}
+
+if(strlen($nome) < 3)
+{
+    echo 'O nome deve possuior mais que 3 caracteres';
+}    
+
+if(strlen($nome) > 40)
+{
+    echo 'O nome é muito extenso';
+}
+
+if(!is_numeric($idade))
+{
+    echo 'Informe um numero valido';
+    return;
+}
 
 //var_dump($nome);
 //var_dump($idade);
@@ -17,7 +38,7 @@ if($idade >= 6 && $idade <= 12)
     for($i = 0; $i <= count($categorias); $i++)
         {
             if($categorias[$i] == 'infantil')
-            echo "O nadador " .$nome. "compete na categoria infantil";
+            echo "O nadador " .$nome. " compete na categoria infantil";
         }
 }
 
@@ -26,7 +47,7 @@ else if($idade >= 13 && $idade <= 18)
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adolescentes')
-        echo "O nadador " .$nome. "compete na categoria adolescentes";
+        echo "O nadador " .$nome. " compete na categoria adolescentes";
     }
 }
 else
@@ -34,6 +55,6 @@ else
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adulto')
-        echo "O nadador " .$nome. "compete na categoria adulto";
+        echo "O nadador " .$nome. " compete na categoria adulto";
     }
 }
